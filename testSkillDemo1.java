e3dbb11a0b9783889019ddc52fd66acf66d09219
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-30 00:33:29 -0700

    Update testSkillDemo1.java

--- testSkillDemo1.java
+++ testSkillDemo1.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class testSkillDemo1 {
     @Test
     public void multiplication() {
-        assertEquals(0, skillDemo1.multiply(1, 2));
+        assertEquals(2, skillDemo1.multiply(1, 2));
     }
 }
