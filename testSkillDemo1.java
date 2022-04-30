ffb06824d1789d0a2bb77166cbd310974f76181b
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-30 00:32:01 -0700

    Create testSkillDemo1.java

---
+++ testSkillDemo1.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class testSkillDemo1 {
+    @Test
+    public void multiplication() {
+        assertEquals(0, skillDemo1.multiply(1, 2));
+    }
+}
